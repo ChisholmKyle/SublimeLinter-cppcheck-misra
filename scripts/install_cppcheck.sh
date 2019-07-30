332a214d1309fac75a2fc9fe44150ce9084a0d52
Kyle Chisholm <dev@kylechisholm.ca>
2019-07-30 15:55:21 -0400

    update install script for latest cppcheck build

--- scripts/install_cppcheck.sh
+++ scripts/install_cppcheck.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 prog_name=$(basename "${0}")
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,6 +10,7 @@ usage() {
     echo "Usage: ${prog_name} [ args ]"
     echo "args (optional):"
     echo "    --prefix [ install prefix ]     : default '${PREFIX}'"
+    echo "    --version [ cppcheck version ]  : default '${CPPCHECK_VERSION}'"
     echo ""
     echo "This script will build and install cppcheck with misra.py addon"
     echo "from master branch of https://github.com/danmar/cppcheck."
@@ -20,6 +23,7 @@ usage() {
 defaults() {
 
     PREFIX=/usr/local
+    CPPCHECK_VERSION=latest
 
 }
 
@@ -39,6 +43,14 @@ while [[ ${#} -ge 1 && ${1::1} == '-' ]]; do
             fi
             shift
             ;;
+        '--version')
+            if [[ ${#} -eq 1 ]] ; then
+                usage
+            else
+                CPPCHECK_VERSION="$2"
+            fi
+            shift
+            ;;
         * )
             usage
             ;;
@@ -53,6 +65,9 @@ echo "1. Checking out cppcheck repository to /tmp/cppcheck ..."
 cd /tmp
 git clone https://github.com/danmar/cppcheck
 cd cppcheck
+if [ "${CPPCHECK_VERSION}" != "latest" -a "${CPPCHECK_VERSION}" != "master" ] ; then
+    git checkout tags/${CPPCHECK_VERSION}
+fi
 
 echo "2. Generating build files ..."
 mkdir build && cd build
@@ -62,9 +77,9 @@ echo "3. Build and Install ..."
 PREFIX=${PREFIX} make
 sudo make install
 
-echo "4. Copying misra.py to '${PREFIX}/share/CppCheck/addons/misra.py'"
-sudo mkdir -p "${PREFIX}/share/CppCheck/addons"
-sudo cp -f ../addons/misra.py "${PREFIX}/share/CppCheck/addons/"
+echo "4. Copying addons and misra.py to '${PREFIX}/share/CppCheck/addons'"
+sudo mkdir -p "${PREFIX}/share/CppCheck"
+sudo cp -rf ../addons "${PREFIX}/share/CppCheck/"
 
 echo "------------------------------------------------------------"
-echo "Done.'
+echo "Done."
